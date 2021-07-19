38e3df5efd4d6365d9da1fd1d4d8fed23a0d74b0
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2021-07-19 11:02:54 -0400

    Add files via upload

---
+++ basicalgorthmsc6.js
@@ -0,0 +1,7 @@
+//Challenge 6
+function confirmEnding(str, target) {
+
+    return str.slice(str.length - target.length) === target;
+  }
+  
+  confirmEnding("He has to give me a new name", "name");
