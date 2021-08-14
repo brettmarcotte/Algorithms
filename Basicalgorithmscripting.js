c594bef2f8448d13e3877dc5cce6b63612ee4ef2
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2021-08-14 11:35:04 -0400

    Add files via upload

---
+++ Basicalgorithmscripting.js
@@ -0,0 +1,16 @@
+//Challenge 1
+function convertToF(celsius) {
+    let fahrenheit = celsius * (9 / 5) + 32;
+    return fahrenheit;
+  }
+  
+  convertToF(30);
+  //Challenge 2
+  function reverseString(str) {
+    for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+  }
+  //Challenge 3
+  
