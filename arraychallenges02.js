1214254b394dba5d54e5531f1a99353e6fcf5cf3
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2021-08-21 11:09:07 -0400

    Add files via upload

---
+++ arraychallenges02.js
@@ -0,0 +1,42 @@
+//first challenge
+function addition(a, b) {
+    var sum = 5 + 5
+    return sum
+}
+
+//second challenge
+function calcAge(age) {
+    var age = 25
+    return age * 365
+}
+
+//third challenge
+function valueAt(arr, i) {
+    var myArray = [1, 2, 3] {
+        myArray[0]
+    }
+    var myData = myArray[0]
+}
+
+//fourth challenge
+function valueAt(arr, i) {
+    var myArray = [1, 2, 3] {
+        myArray[0]
+    }
+    var myData = myArray[2]
+}
+
+//fifth challenge
+function valueAt(arr, i) {
+    var z = Math.floor(i)
+    return arr[z]
+}
+
+//sixth challenge
+function arrayToString(arr) {
+    var result = "";
+    for (var i = 0; i < arr.length; i++) {
+        result = result + arr[i];
+    }
+    return result;
+}
