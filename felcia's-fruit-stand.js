c9a686583f298851feef181d717dab50a59fd845
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2021-09-02 01:09:44 -0400

    Add files via upload

---
+++ felcia's-fruit-stand.js
@@ -0,0 +1,27 @@
+var fruitNames = ["Apples", "Oranges", "Grapes"]
+var fruitType: [
+    ["Granny Smith", "Golden", "Macintosh"],
+    ["Navel", "clementine", "Mandarin"],
+    ["red", "concord", "moon drops"]
+]
+var fruitPrices: [
+    [2, 3, 3],
+    [3, 2, 3],
+    [1, 2, 1]
+]
+var salesQuantity: [
+    [30, 35, 28],
+    [41, 39, 40],
+    [28, 29, 27]
+]
+var totalSales: [
+        [fruitType = fruitPrices * salesQuantity]
+    ]
+    //var totalSales: [
+    //    [fruitType = fruitPrices * salesQuantity]
+]
+//var totalNumberSold: [
+//    [salesQuantity ]
+]
+//var totalProfit: [
+//    [fruitType = fruitPrices * salesQuantity]
