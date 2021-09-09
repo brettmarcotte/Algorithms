742da442216a97f5699987ba6b4c593a996b188b
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2021-09-09 12:26:23 -0400

    Add files via upload

---
+++ cdroadtrip-mobcode.js
@@ -0,0 +1,13 @@
+//mob coding
+
+let riders = [
+    ["Arnell", "Margie"],
+    ["John", "Jeff", "Brett"],
+    ["Rich", "Taylor", "Simon"],
+    ["Mike", "Shaad", "Gabriel"],
+    ["Elvis", "Jackie", "Pierre"]
+]
+
+console.log(riders[3][2])
+console.log(riders[0][0])
+console.log(riders[0][1])
